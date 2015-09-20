ec78ce4e4c71e8502c32ace84836e727d08205f2
Michal Borek <michal@greenpath.pl>
2015-09-20 10:20:49 +0200

    Refactor DockerRunTask

--- src/main/groovy/pl/greenpath/gradle/task/DockerRunTask.groovy
+++ src/main/groovy/pl/greenpath/gradle/task/DockerRunTask.groovy
@@ -4,17 +4,26 @@ class DockerRunTask extends AbstractDockerTask {
 
   @Override
   protected void prepareExecution() {
-    args(['run'] + (project.extensions.docker.runDetached ? ['-d'] : []) +
-        (project.extensions.docker.port > 0 ? ['-p', getPortMapping()] : []) + getLinkedMicroservices() + project.extensions.docker.runExtraArgs +
-        ['--name=' + getContainerName(), getImageName()])
-    println 'Running container: ' + getContainerName() + ' args: ' + getArgs()
+    args(['run'] + runDetached() + publishedPorts() + links() + extraArgs())
+    println "Running container: ${getContainerName()} args: ${getArgs()}"
   }
 
-  private List<String> getLinkedMicroservices() {
-    return project.extensions.docker.linkedMicroservices.collect {
-      def linkName = it.replaceAll('/', '-')
-      "--link=$linkName:$linkName"
-    }
+  private List<String> links() {
+    project.extensions.docker.linkedMicroservices
+        .collect { it.replaceAll('/', '-') }
+        .collect { "--link=$it:$it" }
+  }
+
+  private List<String> publishedPorts() {
+    project.extensions.docker.port > 0 ? ['-p', getPortMapping()] : []
+  }
+
+  private List<String> runDetached() {
+    project.extensions.docker.runDetached ? ['-d'] : []
+  }
+
+  private List<String> extraArgs() {
+    project.extensions.docker.runExtraArgs + ['--name=' + getContainerName(), getImageName()]
   }
 
 }
